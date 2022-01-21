432b45bc99aa36d691be69eb82145840d7154ba7
aticio <aticiozgur@gmail.com>
2022-01-21 23:12:30 +0300

    Balance business functions inits

---
+++ Balance/domestic/balanceserver/getbalance.go
@@ -0,0 +1,8 @@
+package balanceserver
+
+import "fmt"
+
+func getBalance() {
+	// API KEY and SECRET will be put into the env vars.
+	fmt.Println("soon...")
+}
--- Balance/domestic/balanceserver/server.go
+++ Balance/domestic/balanceserver/server.go
@@ -4,11 +4,35 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+	"github.com/tkanos/gonfig"
 )
 
+type Configuration struct {
+	BinanceAccountUrl string
+	Base              string
+	Quote             string
+}
+
+var configuration = Configuration{}
+
+func init() {
+	abs, err := filepath.Abs("./config.json")
+	if err != nil {
+		log.Error(err)
+	}
+
+	err = gonfig.GetConf(abs, &configuration)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
 func GetBalance(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	getBalance()
 	var b bytes.Buffer
 	b.WriteString("soon...")
 	w.Header().Set("Content-Type", "text/plain")
