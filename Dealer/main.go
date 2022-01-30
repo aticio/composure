a022b80e652f18aa5fa2712d086b51af7bf05f32
aticio <aticiozgur@gmail.com>
2022-01-30 10:34:35 +0300

    Docker file added for Dealer service

--- Dealer/main.go
+++ Dealer/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	server "composure/Dealer/domestic/dealerserver"
+
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	router := httprouter.New()
+	router.POST("/getDeal", server.GetDeal)
+	log.Info("starting Dealer server on port 8084")
+	log.Fatal(http.ListenAndServe(":8084", router))
 }
